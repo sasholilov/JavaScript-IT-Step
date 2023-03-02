60d50c2c8e35283e75fdfff79a77751f6cb4aff6
sasholilov <alex_xld@abv.bg>
2023-03-02 22:13:59 +0200

    Add Profile

---
+++ Profile/app.js
@@ -0,0 +1,11 @@
+function lockedProfile() {
+  // In this problem, you should create a JS functionality that shows and hides the additional information about users.
+
+  // When one of the [Show more] buttons is clicked, the hidden information inside the div should
+  // be shown, only if the profile is not locked! If the current profile is locked, nothing should happen.
+
+  // If the hidden information is displayed and we lock the profile again, the [Hide it] button should not be working!
+  // Otherwise, when the profile is unlocked and we click on the [Hide it] button, the new fields must hide again.
+
+  console.log("TODO...");
+}
