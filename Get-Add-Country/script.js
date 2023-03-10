38b2560b8029cb47652ec314b458902db214d54d
sasholilov <alex_xld@abv.bg>
2023-03-11 01:01:15 +0200

    Update

--- Get-Add-Country/script.js
+++ Get-Add-Country/script.js
@@ -15,15 +15,35 @@ const getCountry = async function () {
     console.log(data.flags.png);
     const renderCountry = function () {
       const div = document.createElement('div');
-      div.classList.add('country');
       const img = document.createElement('img');
+      const countryName = document.createElement('h2');
+      const populationEl = document.createElement('p');
+      const cityEl = document.createElement('p');
+      const regionEl = document.createElement('p');
+      const buttonEl = document.createElement('button');
+      div.classList.add('country');
       img.setAttribute('src', `${data.flags.svg}`);
       img.classList.add('flag-image');
       div.appendChild(img);
-      const countryName = document.createElement('h2');
       countryName.textContent = data.name.common;
       div.appendChild(countryName);
+      populationEl.innerHTML = `<i class="fa-solid fa-person"></i>Population: ${data.population}`;
+      cityEl.innerHTML = `<i class="fa-solid fa-city"></i>Capital city: ${data.capital}`;
+      regionEl.innerHTML = `<i class="fa-solid fa-earth-americas"></i>Region: ${data.region}`;
+      buttonEl.textContent = 'Show more';
+      buttonEl.classList.add('btncountry');
+      div.appendChild(populationEl);
+      div.appendChild(cityEl);
+      div.appendChild(regionEl);
+      div.appendChild(buttonEl);
       main.appendChild(div);
+
+      buttonEl.addEventListener('click', function () {
+        main.style.display = 'none';
+        //render more info block
+        alert('render more info block');
+      });
+
       inputCountryName.value = '';
     };
     renderCountry();
