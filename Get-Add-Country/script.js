c738cac27cb5751b30cbbd0e68cab7942eca1a66
sasholilov <alex_xld@abv.bg>
2023-03-14 21:37:27 +0200

    Added languages data and error render

--- Get-Add-Country/script.js
+++ Get-Add-Country/script.js
@@ -5,10 +5,31 @@ const countryEl = document.querySelector('.country');
 const btnAdd = document.getElementById('addcountry');
 const main = document.getElementById('main');
 const moreInfoEl = document.querySelector('.moreinfo');
+const errorEl = document.querySelector('.error');
 const btnCloseInfo = document.querySelector('.close-moreinfo');
 const inputEl = document.querySelector('.input-name');
-let countryArr = [];
 let data = null;
+let countryStorage = [];
+const getLanguages = function (langObj) {
+  const languageKeys = Object.keys(langObj.languages);
+  const languageNames = languageKeys
+    .map(key => langObj.languages[key])
+    .join(', ');
+  return languageNames;
+};
+
+const renderError = function (error) {
+  main.style.display = 'none';
+  inputEl.classList.add('hidden');
+  moreInfoEl.classList.remove('hidden');
+  btnCloseInfo.addEventListener('click', function () {
+    moreInfoEl.classList.add('hidden');
+    main.style.display = 'block';
+    inputEl.classList.remove('hidden');
+  });
+  moreInfoEl.childNodes[3].textContent = error.message;
+  console.log(error);
+};
 const moreInfo = function () {
   main.style.display = 'none';
   inputEl.classList.add('hidden');
@@ -25,13 +46,13 @@ const getCountry = async function () {
     const response = await fetch(
       `https://restcountries.com/v3.1/name/${inputCountryName.value}`
     );
-    if (!response.ok) {
-      throw new Error(response.status);
+    if (response.status == 404) {
+      console.log(response);
+      throw new Error('The country does not exist. Try again!');
     }
     const dataArr = await response.json();
     data = dataArr[0];
-    countryArr.push(dataArr[0]);
-    console.log(data.flags.png);
+    countryStorage.push(data);
     const renderCountry = function () {
       const div = document.createElement('div');
       const img = document.createElement('img');
@@ -39,7 +60,7 @@ const getCountry = async function () {
       const populationEl = document.createElement('p');
       const cityEl = document.createElement('p');
       const regionEl = document.createElement('p');
-      const buttonEl = document.createElement('button');
+      const buttonSwohMore = document.createElement('button');
       const buttonRemoveEl = document.createElement('button');
       div.classList.add('country');
       img.setAttribute('src', `${data.flags.svg}`);
@@ -50,24 +71,21 @@ const getCountry = async function () {
       populationEl.innerHTML = `<i class="fa-solid fa-person"></i>Population: ${data.population}`;
       cityEl.innerHTML = `<i class="fa-solid fa-city"></i>Capital city: ${data.capital}`;
       regionEl.innerHTML = `<i class="fa-solid fa-earth-americas"></i>Region: ${data.region}`;
-      buttonEl.textContent = 'Show more';
-      buttonEl.classList.add('btncountry');
+      buttonSwohMore.textContent = 'Show more';
+      buttonSwohMore.classList.add('btncountry');
       buttonRemoveEl.textContent = 'Remove';
       buttonRemoveEl.classList.add('btncountry');
       div.appendChild(populationEl);
       div.appendChild(cityEl);
       div.appendChild(regionEl);
-      div.appendChild(buttonEl);
+      div.appendChild(buttonSwohMore);
       div.appendChild(buttonRemoveEl);
       main.appendChild(div);
 
-      buttonEl.addEventListener('click', function (event) {
+      buttonSwohMore.addEventListener('click', function (event) {
         moreInfo();
-        let findCountry =
-          event.target.previousSibling.previousSibling.previousSibling
-            .previousSibling.textContent;
-        console.log(findCountry);
-        const filteredCountryArr = countryArr.filter(
+        let findCountry = event.target.parentElement.childNodes[1].textContent;
+        const filteredCountryArr = countryStorage.filter(
           e => e.name.common == findCountry
         );
         console.log(filteredCountryArr);
@@ -75,22 +93,23 @@ const getCountry = async function () {
         moreInfoEl.childNodes[3].textContent =
           filteredCountryArr[0].name.common;
         moreInfoEl.childNodes[5].textContent = `Official: ${filteredCountryArr[0].name.official}`;
-        moreInfoEl.childNodes[6].textContent = `Languages: ${filteredCountryArr[0].languages}`;
+        moreInfoEl.childNodes[6].textContent = `Languages: ${getLanguages(
+          filteredCountryArr[0]
+        )}`;
         moreInfoEl.childNodes[7].textContent = `Area: ${filteredCountryArr[0].area}`;
         moreInfoEl.childNodes[8].textContent = `Google Maps: ${filteredCountryArr[0].maps.googleMaps}`;
         moreInfoEl.childNodes[9].textContent = `Fifa: ${filteredCountryArr[0].fifa}`;
-        moreInfoEl.childNodes[10].textContent = `Cars: ${filteredCountryArr[0].car.signs}`;
+        moreInfoEl.childNodes[10].textContent = `Cars License: ${filteredCountryArr[0].car.signs}`;
         moreInfoEl.childNodes[11].textContent = `Coordinates: ${filteredCountryArr[0].capitalInfo.latlng}`;
       });
 
       buttonRemoveEl.addEventListener('click', function (event) {
         if (window.confirm('Do you confirm?')) {
           let findCountry =
-            event.target.previousSibling.previousSibling.previousSibling
-              .previousSibling.previousSibling.textContent;
-          console.log(findCountry);
-          countryArr = countryArr.filter(e => e.name.common != findCountry);
-          console.log(countryArr);
+            event.target.parentElement.childNodes[1].textContent;
+          countryStorage = countryStorage.filter(
+            e => e.name.common != findCountry
+          );
           buttonRemoveEl.parentElement.remove();
         }
       });
@@ -99,8 +118,8 @@ const getCountry = async function () {
     };
     renderCountry();
   } catch (error) {
-    alert(error);
+    renderError(error);
   }
 };
 btnAdd.addEventListener('click', getCountry);
-console.log(countryArr);
+console.log(countryStorage);
