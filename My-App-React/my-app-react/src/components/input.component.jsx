bd9ce67afdc0d058beafd26456b7034fe62f8e4a
sasholilov <alex_xld@abv.bg>
2023-04-21 01:50:11 +0300

    Add countries in Array
    Add countries in Array whe click to Add buton

--- My-App-React/my-app-react/src/components/input.component.jsx
+++ My-App-React/my-app-react/src/components/input.component.jsx
@@ -2,13 +2,21 @@
 import "./styles.css";
 import React, { useState, useEffect, Fragment } from "react";
 import Country from "./country.component";
+import CountryList from "./country-list.component";
 
 const InputComponent = () => {
   const [inputField, setInputField] = useState("");
+  const [countryArr, setCountryArr] = useState([]);
   const [country, setcountries] = useState(null);
   const onChangeHandler = (event) => {
     setInputField(event.target.value);
     console.log(event.target.value);
+  };
+
+  const onClickHandler = () => {
+    if (country) {
+      setCountryArr([...countryArr, country]); // Use spread operator to push country into countryArr immutably
+    }
   };
 
   useEffect(() => {
@@ -17,7 +25,13 @@ const InputComponent = () => {
       .then((country) => setcountries(country[0]));
   }, [inputField]); // Update fetch call when inputField changes
 
-  console.log(country);
+  // useEffect(() => {
+  //   if (country) {
+  //     setCountryArr([...countryArr, country]); // Use spread operator to push country into countryArr immutably
+  //   }
+  // }, [country]); // Update countryArr when country changes
+
+  console.log(countryArr);
 
   return (
     <Fragment>
@@ -28,11 +42,11 @@ const InputComponent = () => {
           value={inputField}
           onChange={onChangeHandler}
         />
-        <button id="addcountry" className="btncountry">
+        <button id="addcountry" className="btncountry" onClick={onClickHandler}>
           Add
         </button>
       </div>
-      {country && <Country country={country} />}
+      {country && <CountryList country={country} />}
     </Fragment>
   );
 };
